2c7536e9767cb0767575b3bf1bc6b8f0662eaecd
Kaua Santtos Souza <113460644+KauaSanttosSouza@users.noreply.github.com>
2023-03-24 19:20:16 -0300

    Add files via upload

---
+++ SPANUniverse/index.js
@@ -0,0 +1,14 @@
+import { Router } from "./router.js"
+
+const router = new Router
+
+router.add("/ico", "/home.html")
+router.add("/home", "/home.html")
+router.add("/universe", "/universe.html")
+router.add("/explorer", "/explorer.html")
+router.add("/button", "/universe.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
